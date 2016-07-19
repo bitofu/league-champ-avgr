34e3d55c14fb695892ae839a1c25fc25800294be
bittertofu <vincent.sungasong@gmail.com>
2016-07-19 03:59:50 -0700

    heroku

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var fetch = require('node-fetch');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index');
+});
+
 app.get('/get-summoner', function (req, res) {
   var name = req.query.summonerName.trim().toLowerCase().replace(' ', '');
   console.log('get-summoner: ' + name)
